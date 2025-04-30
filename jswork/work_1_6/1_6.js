6638f75c2833e5b85be54194e850fb8f835a91a3
katsuno <ani9.18ap@gmail.com>
2025-04-30 22:17:33 +0900

    1_6課題に自己解説を追加、index.htmlのタイトル修正

--- jswork/work_1_6/1_6.js
+++ jswork/work_1_6/1_6.js
@@ -16,7 +16,7 @@ function makeCoffee() {
 
   }
 
-  //まだ関数を定義しただけ。実行はされない。
+  //まだ関数を定義しただけ。実行はされない。同時だけど先に呼ばれてるので３番目。
 
   function makeSandwich() {
 
@@ -36,18 +36,18 @@ function makeCoffee() {
 
   }
 
-  //まだ関数を定義しただけ。実行はされない。
+  //まだ関数を定義しただけ。実行はされない。同時だけど後に呼ばれてるので４番目。
 
   async function serveCustomer() {
 
     await Promise.all([makeCoffee(),makeSandwich()]);
 
-    console.log("🍽️ スタッフ：すべてできたので配膳します！");
+    console.log("🍽️ スタッフ：すべてできたので配膳します！"); //これが５番目。
 
   }
 
   
 
-  console.log("🥪 お客さんがコーヒーとサンドイッチを注文しました");
+  console.log("🥪 お客さんがコーヒーとサンドイッチを注文しました"); //一番最初に実行されるよ！
 
-  serveCustomer();
+  serveCustomer();//これが２番目、serverCustomerの関数が実行される。これはasyncのところで定義してあるのでそれが実行される。asyncとawaitがあるのでその中身の処理（makeCoffee(),makeSandwich())が終わってからの実行となりまだ出力されない状態。そしてPromise.allがあるのでそのなかの配列の関数がすべて実行されるまで待機される。
